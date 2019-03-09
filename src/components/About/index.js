5dc31e4991c2d4c39162b92daecc1975309743ec
jfecke <jfecke@gmail.com>
2019-03-09 11:43:36 -0600

    changed more capitalization for heroku

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About(props) {
     <div  className="aboutme">
       <div className="ContainerofBtn">
         <a href="https://www.linkedin.com/in/joelfecke/" className="leftpos">
-          <img  src={process.env.PUBLIC_URL + "/assets/images/linkedin.png"} alt="#" />
+          <img  src={process.env.PUBLIC_URL + "/assets/images/linkedIn.png"} alt="#" />
         </a>
         <a href="https://github.com/jfecke" className="rightpos">
           <img src={process.env.PUBLIC_URL + "/assets/images/github.png"} alt="#" />
