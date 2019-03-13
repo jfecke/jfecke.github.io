e1896bd18555d515acf5eb3545b3ef37858c10db
jfecke <jfecke@gmail.com>
2019-03-13 16:52:04 -0500

    added resume button

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,10 +5,13 @@ function About(props) {
   return (
     <div  className="aboutme">
       <div className="ContainerofBtn">
-        <a href="https://www.linkedin.com/in/joelfecke/" className="leftpos">
-          <img  src={process.env.PUBLIC_URL + "/assets/images/linkedIn.png"} alt="#" />
+        <a href="https://www.linkedin.com/in/joelfecke/" target="_blank" rel="noopener noreferrer" className="card card2">
+          <img src={process.env.PUBLIC_URL + "/assets/images/linkedIn.png"}alt="#" />
         </a>
-        <a href="https://github.com/jfecke" className="rightpos">
+        <a href="https://docs.google.com/document/d/1tWfq-OUi5l_meuzdo1xfOeoL4lNlJ4hA41YoQEdAeVc/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="card card2">
+          <img src={process.env.PUBLIC_URL + "/assets/images/resume.png"} alt="#" />
+        </a>
+        <a href="https://github.com/jfecke" target="_blank" rel="noopener noreferrer" className="card card2">
           <img src={process.env.PUBLIC_URL + "/assets/images/github.png"} alt="#" />
         </a>
       </div>
