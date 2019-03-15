bced1548c704ee5ac759db230bde75a524972ea8
jfecke <jfecke@gmail.com>
2019-03-14 21:27:17 -0500

    updated resume link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,7 +22,7 @@ function About(props) {
         <a href="https://www.linkedin.com/in/joelfecke/" target="_blank" rel="noopener noreferrer" className="card card2">
           <img src={process.env.PUBLIC_URL + "/assets/images/linkedIn.png"}alt="#" />
         </a>
-        <a href="https://docs.google.com/document/d/1tWfq-OUi5l_meuzdo1xfOeoL4lNlJ4hA41YoQEdAeVc/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="card card2">
+        <a href="https://docs.google.com/document/d/1abIBjzu3yoyZ5uRh8RDppmaRIjdyyEZE7z_2fLDTnjM/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="card card2">
           <img src={process.env.PUBLIC_URL + "/assets/images/resume.png"} alt="#" />
         </a>
         <a href="https://github.com/jfecke" target="_blank" rel="noopener noreferrer" className="card card2">
