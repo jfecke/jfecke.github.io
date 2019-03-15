9ebd8d60a34c1f32641bd00001769b65d42f85e8
jfecke <jfecke@gmail.com>
2019-03-14 19:40:12 -0500

    added photo and summary

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,6 +5,23 @@ import "./style.css";
 function About(props) {
   return (
     <Wrapper>
+        <div className="aboutme"> 
+        <div className="card mypic">
+          <img src={process.env.PUBLIC_URL + "/assets/images/joel.jpg"} alt="#" />
+        </div>
+        <div className="card mybio scrollbar scrollbar-black bordered-black square thin">
+          <div class="force-overflow">
+          Full Stack Web Developer with a passion for object-oriented programing. Currently a Certified Scrum Master with 
+          a BS in Mechanical Engineering. Experienced project manager with a proven record of leadership and execution of 
+          complex and challenging technical projects. Diverse military and civilian background spanning multiple disciplines 
+          and industries. Strong interpersonal and communication skills utilized to lead and motivate cross-functional teams 
+          to achieve stretch goals. Creative and innovative problem solver with relentless focus on learning new skills.
+          </div>
+        
+        </div>
+        </div>
+        
+        
         <a href="https://www.linkedin.com/in/joelfecke/" target="_blank" rel="noopener noreferrer" className="card card2">
           <img src={process.env.PUBLIC_URL + "/assets/images/linkedIn.png"}alt="#" />
         </a>
